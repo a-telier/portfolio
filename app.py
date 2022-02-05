5d4a907dc67910ad6765ace3907d1806c89e4057
a-telier <Alexandra.rabi@outlook.com>
2022-02-05 23:45:30 +0000

    base.html file added to production

--- app.py
+++ app.py
@@ -9,5 +9,8 @@ import datetime
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/home')
+def index():
+    return render_template("index.html")
+
+# RUN in production by using run --host=0.0.0.0
